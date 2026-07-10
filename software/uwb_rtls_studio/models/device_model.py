5c73be4b99957d55e32ba57fe337bc4d9ce1c503
Trung Quân <quannn13112020@gmail.com>
2026-07-10 16:19:30 +0700

    refactor: fix cant show session record data

--- software/uwb_rtls_studio/models/device_model.py
+++ software/uwb_rtls_studio/models/device_model.py
@@ -160,6 +160,11 @@ class DeviceModel(QObject):
         self._ble_status_timer.setInterval(10000)
         self._ble_status_timer.timeout.connect(self._poll_ble_status)
 
+        # RTOS task stats poll timer (5s interval)
+        self._rtos_task_stats_timer = QTimer(self)
+        self._rtos_task_stats_timer.setInterval(5000)
+        self._rtos_task_stats_timer.timeout.connect(lambda: self.request_rtos_task_stats(force=True))
+
         # battery_info is received via device telemetry stream (1s push); no host-side poll timer needed.
 
         self._ble_transition_timer = QTimer(self)
@@ -737,8 +742,11 @@ class DeviceModel(QObject):
         if enabled:
             if not self._ble_status_timer.isActive():
                 self._ble_status_timer.start()
+            if not self._rtos_task_stats_timer.isActive():
+                self._rtos_task_stats_timer.start()
             return
         self._ble_status_timer.stop()
+        self._rtos_task_stats_timer.stop()
 
     def _schedule_background_scan_after_connect(self) -> None:
         # Disable automatic background scan after connect by user request
@@ -1529,6 +1537,7 @@ class DeviceModel(QObject):
         self._connect_generation += 1
         self._prune_timer.stop()
         self._ble_status_timer.stop()
+        self._rtos_task_stats_timer.stop()
         self._ble_transition_timer.stop()
         self._connect_timeout_timer.stop()
         self._background_scan_resume_timer.stop()
@@ -1585,6 +1594,8 @@ class DeviceModel(QObject):
         elif param_name == "device_type_set":
             if self._config_repo is None:
                 self._handle_device_type(pkt.device_type_set)
+        elif param_name == "anchor_layout_resp":
+            self._mark_query_received("anchor_layout_resp")
 
 
     def _on_repository_battery_info(self, info: dict) -> None:
@@ -1863,6 +1874,7 @@ class DeviceModel(QObject):
                 self._handshake_time_sync_timer.stop()
                 self._pending_handshake_time_sync_seq = None
             self._ble_status_timer.stop()
+            self._rtos_task_stats_timer.stop()
             self._ble_transition_timer.stop()
             self._connect_timeout_timer.stop()
             self._background_scan_resume_timer.stop()
